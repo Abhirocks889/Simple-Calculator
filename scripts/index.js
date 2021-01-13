1d1aa8f4094fbefaddc84dc4d072e6ea2554af2d
Abhishek <aburagohain889@gmail.com>
2021-01-13 22:07:07 +0530

    feat: Added basic calculator functionality. Add, subtract, multiply, divide with clear

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,9 @@
+let displayResult = '';
+let operand1 = '';
+let operator = '';
+let operand2 = '';
+let operand1Set = false;
+
 const result =  document.querySelector('.result');
 
 /**
@@ -7,6 +13,46 @@ const result =  document.querySelector('.result');
  */
 function onButtonClick(value) {
     if (!!result) {
-        result.innerHTML = value;
+        if (isOperator(value)) {
+            operator = value;
+            operand1Set = true;
+            displayResult = operator;
+        } else if (value === '=') {
+            displayResult = calculate(operand1, operand2, operator);
+            operand2 = '';
+            operand1 = '';
+            operator = '';
+            operand1Set = false;
+        } else {
+            if (!operand1Set) {
+                operand1 += value;
+                displayResult = operand1;
+            } else {
+                operand2 += value;
+                displayResult = operand2;
+            }
+        }
+        result.innerHTML = displayResult;
+    }
+}
+
+function isOperator(value) {
+    return value === '/' || value === 'X' || value === '+' || value === '-';
+}
+
+function calculate(operand1, operand2, operator) {
+    let op1 = operand1 !== '' ? parseInt(operand1) : 0;
+    let op2 = operand2 !== '' ? parseInt(operand2) : 0;
+    switch (operator) {
+        case '+':
+            return op1 + op2;
+        case '-':
+            return op1 - op2;
+        case 'X':
+            return op1 * op2;
+        case '/':
+            return op1 / op2;
+        default:
+            return 0;
     }
 }
