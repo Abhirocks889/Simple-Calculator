8cba1c50cd4be54cc6777ef89dd771fe3a894836
Abhishek <aburagohain889@gmail.com>
2021-02-01 21:42:05 +0530

    feat: abstracted code into a closure and completed functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,5 @@
-let displayResult = '';
-let operand1 = '';
-let operator = '';
-let operand2 = '';
-let operand1Set = false;
-
 const result = document.querySelector('.result');
+const getResult = calculator();
 
 /**
  * Called on every button click
@@ -17,14 +12,55 @@ function onButtonClick(value) {
 }
 
 /**
- * Returns the result to be displayed on the screen
- * @param value
- * @returns string
+ * Creates a closure that creates an
+ * abstraction for the calculator methods
  */
-function getResult(value) {
-  let displayResult;
+function calculator() {
+  let displayResult = '', operand1 = '', operator = '';
+  let operand2 = '', operand1Set = false;
   
-  if (isOperator(value)) {
+  /**
+   * Returns the result to be displayed on the screen
+   * @param value
+   * @returns strings
+   */
+  return function getResult(value) {    
+    if (isOperator(value)) {
+      return getResultForOperator(value);
+    } else if (value === '=') {
+      displayResult = calculate(operand1, operand2, operator);
+      resetToInitialState();
+      return displayResult;
+    } else if (value === 'CE') {
+      resetToInitialState();
+      return 'Result';
+    } else {
+      return getResultForOperand(value);
+    }
+  }
+  
+  /**
+   * Gets the display result for a given operand
+   * @param value
+   * @returns string
+   */
+  function getResultForOperand(value) {
+    if (!operand1Set) {
+      operand1 += value;
+      displayResult = operand1;
+    } else {
+      operand2 += value;
+      displayResult = `${operand1} ${operator} ${operand2}`;;
+    }
+    return displayResult;
+  }
+  
+  /**
+   * Gets the display result for a given operator
+   * @param value
+   * @returns string
+   */
+  function getResultForOperator(value) {
     if (operand1Set) {
       const computed = calculate(operand1, operand2, operator);
       operand1 = computed;
@@ -34,65 +70,51 @@ function getResult(value) {
     } else {
       operand1Set = true;
       operator = value;
-      operand1 = operand1 === '' ? 0 : operand1;
+      operand1 = (typeof displayResult === 'number') ? displayResult : operand1 === '' ? 0 : operand1;
       displayResult = `${operand1} ${operator}`;
     }
-  } else if (value === '=') {
-    displayResult = calculate(operand1, operand2, operator);
-    resetToInitialState();
-  } else if (value === 'CE') {
-    displayResult = 'Result';
-    resetToInitialState();
-  } else {
-    if (!operand1Set) {
-      operand1 += value;
-      displayResult = operand1;
-    } else {
-      operand2 += value;
-      displayResult = `${operand1} ${operator} ${operand2}`;;
-    }
+    return displayResult;
   }
-  return displayResult;
-}
+  
+  /**
+   * Resets the operator and the operands to the initial state
+   */
+  function resetToInitialState() {
+    operand1 = '';
+    operator = '';
+    operand2 = '';
+    operand1Set = false;
+  }
+  
+  /**
+   * Checks if the value is of operand type
+   * @param value
+   * @returns boolean
+   */
+  function isOperator(value) {
+    return value === '/' || value === 'X' || value === '+' || value === '-';
+  }
 
-/**
- * Resets the operator and the operands to the initial state
- */
-function resetToInitialState() {
-  operand1 = '';
-  operator = '';
-  operand2 = '';
-  operand1Set = false;
-}
-
-/**
- * Checks if the value is of operand type
- * @param value
- * @returns boolean
- */
-function isOperator(value) {
-  return value === '/' || value === 'X' || value === '+' || value === '-';
-}
-
-/**
- * Performs the mathematical calculation
- * of two operands with the specified operator
- * @param value
- * @returns number
- */
-function calculate(operand1, operand2, operator) {
-  operand1 = +operand1;
-  operand2 = +operand2;
-  switch (operator) {
-    case '+':
-      return operand1 + operand2;
-    case '-':
-      return operand1 - operand2;
-    case 'X':
-      return operand1 * operand2;
-    case '/':
-      return operand1 / operand2;
-    default:
-      return 0;
+  /**
+   * Performs the mathematical calculation
+   * of two operands with the specified operator
+   * @param value
+   * @returns number
+   */
+  function calculate(operand1, operand2, operator) {
+    operand1 = +operand1;
+    operand2 = +operand2;
+    switch (operator) {
+      case '+':
+        return operand1 + operand2;
+      case '-':
+        return operand1 - operand2;
+      case 'X':
+        return operand1 * operand2;
+      case '/':
+        return operand1 / operand2;
+      default:
+        return 0;
+    }
   }
 }
